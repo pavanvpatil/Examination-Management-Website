ff15757bcded0d0d6c0464e5e482c227491b59e1
pavanvpatil <pavanvpatil01@gmail.com>
2021-10-17 10:48:06 +0530

    Added navigation bar

--- homepage.php
+++ homepage.php
@@ -1,3 +1,10 @@
+<?php
+    session_start();
+    if(!isset($_SESSION['loggedin']))
+    {
+        header("Location: index.php");
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,18 +12,33 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>homepage</title>
+<style>
+.topnav {
+  overflow: hidden;
+  background-color:#cfcccc;
+}
+
+.topnav a {
+  float: left;
+  color:black;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 22px;
+}
+
+.topnav a:hover {
+  background-color:skyblue;
+  color: black;
+}
+</style>
 </head>
 <body>
-    <?php
-    session_start();
-    if(!isset($_SESSION['loggedin']))
-    {
-        header("Location: index.php");
-    }
-
-
-    ?>
-    <h1>This will be the homepage</h1>
-    
+<div class="topnav">
+  <a href="#home">Home</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a>
+  <a href="index.php" onlclick="<?php session_destroy(); ?>">Log-out</a>
+</div>
 </body>
 </html>
